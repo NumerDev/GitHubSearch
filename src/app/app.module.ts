030ecbd070257fd9c7db6f48522c5af15ce71740
Numer <numerpriv@gmail.com>
2022-03-04 12:35:46 +0100

    Added wildcard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WildcardComponent } from './components/wildcard/wildcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WildcardComponent
   ],
   imports: [
     BrowserModule,
